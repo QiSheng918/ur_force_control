5e38b1abe70043a64f1f945e3519930597d76273
QiSheng918 <403784100@sjtu.edu.cn>
2020-11-22 16:07:57 +0800

    在UR5上测试姿态插补，利用前馈+反馈实现了姿态跟踪

--- ur_control_test/src/intercept_test.cpp
+++ ur_control_test/src/intercept_test.cpp
@@ -20,6 +20,9 @@
 #include <kdl/trajectory_composite.hpp>
 #include <cmath>
 
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
 std::string double2string(double input);
 std::string combinemsg(std::vector<double> velocity, double acc = 1);
 
@@ -40,12 +43,18 @@ int main(int argc, char *argv[])
         KDL::Path_RoundedComposite *path = new KDL::Path_RoundedComposite(0.05, 0.01, new KDL::RotationalInterpolation_SingleAxis());
         if (flag == 0)
         {
-            path->Add(KDL::Frame(KDL::Rotation::Quaternion(-0.541875, -0.480129, 0.460675, 0.513446), KDL::Vector(-0.23073, -0.0927758, 0.647443)));
-            path->Add(KDL::Frame(KDL::Rotation::Quaternion(0.586656,0.587972,-0.340895,-0.440356), KDL::Vector(-0.23073, -0.0927758, 0.647443)));
+                        path->Add(KDL::Frame(KDL::Rotation::Quaternion(-0.192521,-0.348544,0.396922,0.826986), KDL::Vector(-0.23073, -0.0927758, 0.647443)));
+            path->Add(KDL::Frame(KDL::Rotation::Quaternion(-0.322089,-0.60504,0.31081,0.65847), KDL::Vector(-0.372824, -0.25565, 0.552965)));
+            path->Add(KDL::Frame(KDL::Rotation::Quaternion(0.41756,0.796722,-0.181505,-0.397408), KDL::Vector(-0.373234, -0.255303, 0.648539)));
 
-            // path->Add(KDL::Frame(KDL::Rotation::Quaternion(-0.541533, -0.480408, 0.46102, 0.513236), KDL::Vector(-0.373234, -0.255303, 0.648539)));
+            path->Add(KDL::Frame(KDL::Rotation::Quaternion(0.453703,0.882523,-0.0415021,-0.116549), KDL::Vector(-0.373234, -0.255303, 0.848539)));
 
-            // path->Add(KDL::Frame(KDL::Rotation::Quaternion(-0.541875, -0.480129, 0.460675, 0.513446), KDL::Vector(-0.372824, -0.25565, 0.552965)));
+
+
+
+
+
+
         }
         else
         {
@@ -57,7 +66,7 @@ int main(int argc, char *argv[])
         // always call Finish() at the end, otherwise the last segment will not be added.
         path->Finish();
 
-        KDL::VelocityProfile *velpref = new KDL::VelocityProfile_Trap(0.5, 0.1);
+        KDL::VelocityProfile *velpref = new KDL::VelocityProfile_Trap(0.3, 0.1);
         velpref->SetProfile(0, path->PathLength());
         traject = new KDL::Trajectory_Segment(path, velpref);
     }
@@ -104,9 +113,27 @@ int main(int argc, char *argv[])
 
         KDL::Frame target_pose = traject->Pos(t);
         KDL::Twist target_vel = traject->Vel(t);
+        double xd,yd,zd,wd;
+        target_pose.M.GetQuaternion(xd,yd,zd,wd);
+
+		double q_dot=x*xd+y*yd+z*zd+w*wd;
+        
+        if(q_dot<0){
+            x*=-1;
+            y*=-1;
+            z*=-1;
+            w*=-1;
+        }
+
+        Eigen::Matrix<double,3,1> epsilon, epsilon_d;                 
+        epsilon << x,y,z;        
+        epsilon_d <<xd,yd,zd;    
+        Eigen::Matrix<double,3,3> skew_matrix;
+        skew_matrix << 0,-epsilon_d(2),epsilon_d(1),epsilon_d(2),0,-epsilon_d(0),-epsilon_d(1),epsilon_d(0),0;    
+        Eigen::Matrix<double,3,1> orient_error = wd * epsilon - w * epsilon_d + skew_matrix * epsilon;   
         for (int i = 0; i < 3; i++)
-            // command_vel[i+3] = target_vel.rot.data[i] + 0.5 * (target_pose(i, 3) - current_pose[i]);
-            command_vel[i+3] = target_vel.rot.data[i];
+            // command_vel[i] = target_vel.vel.data[i] + 0.5 * (target_pose(i, 3) - current_pose[i]);
+            command_vel[i+3] = target_vel.rot.data[i]-1*orient_error[i];
 
         ur_script_msgs.data = combinemsg(command_vel);
         std::cout << ur_script_msgs.data << std::endl;
